ec2272217c7eaa9a7aba92f2f22daa04c9c0e93a
Burhan Aras <burhanaras@live.com>
2014-01-08 23:15:13 +0200

    Check for win control added.

--- src/com/burhan/tictactoe/TicTacToeGame.java
+++ src/com/burhan/tictactoe/TicTacToeGame.java
@@ -10,7 +10,15 @@ public class TicTacToeGame {
 
 	private char board[] = new char[9];
 	
+	public enum GameStatus{ GAME_GOES_ON, ITS_A_TIE, COMPUTER_WINS, HUMAN_PLAYER_WINS};
+	private GameStatus gameStatus;
+	
+	public TicTacToeGame() {
+		gameStatus = GameStatus.GAME_GOES_ON;
+	}
+	
 	public void clearBoard() {
+		board = new char[9];
 	}
 
 	public void setMove(char player, int location) {
@@ -20,6 +28,61 @@ public class TicTacToeGame {
 		if(board[location] == '\u0000'){
 			board[location] = player;
 		}
+		
+		checkForWin();
+	}
+
+	private void checkForWin() {
+		// Check horizontal wins
+				for (int i = 0; i <= 6; i += 3)	{
+					if (board[i] == HUMAN_PLAYER && 
+						board[i+1] == HUMAN_PLAYER &&
+						board[i+2]== HUMAN_PLAYER)
+						gameStatus = GameStatus.HUMAN_PLAYER_WINS;
+					
+					if (board[i] == COMPUTER_PLAYER && 
+						board[i+1]== COMPUTER_PLAYER && 
+						board[i+2] == COMPUTER_PLAYER)
+						gameStatus = GameStatus.COMPUTER_WINS;
+				}
+			
+				// Check vertical wins
+				for (int i = 0; i <= 2; i++) {
+					if (board[i] == HUMAN_PLAYER && 
+						board[i+3] == HUMAN_PLAYER && 
+						board[i+6]== HUMAN_PLAYER)
+						gameStatus = GameStatus.HUMAN_PLAYER_WINS;
+					
+					if (board[i] == COMPUTER_PLAYER && 
+						board[i+3] == COMPUTER_PLAYER && 
+						board[i+6]== COMPUTER_PLAYER)
+						gameStatus = GameStatus.COMPUTER_WINS;
+				}
+			
+				// Check for diagonal wins
+				if ((board[0] == HUMAN_PLAYER &&
+					 board[4] == HUMAN_PLAYER && 
+					 board[8] == HUMAN_PLAYER) ||
+					(board[2] == HUMAN_PLAYER && 
+					 board[4] == HUMAN_PLAYER &&
+					 board[6] == HUMAN_PLAYER))
+					gameStatus = GameStatus.HUMAN_PLAYER_WINS;
+				
+				if ((board[0] == COMPUTER_PLAYER &&
+					 board[4] == COMPUTER_PLAYER && 
+					 board[8] == COMPUTER_PLAYER) ||
+					(board[2] == COMPUTER_PLAYER && 
+					 board[4] == COMPUTER_PLAYER &&
+					 board[6] == COMPUTER_PLAYER))
+					gameStatus = GameStatus.COMPUTER_WINS;
+			
+				// Check for tie
+				for (int i = 0; i < BOARD_SIZE; i++) {
+					// If we find a number, then no one has won yet
+					if (board[i] != HUMAN_PLAYER && board[i] != COMPUTER_PLAYER)
+						gameStatus = GameStatus.GAME_GOES_ON;
+				}
+			
 	}
 
 	public char[] getBoard() {
@@ -34,4 +97,14 @@ public class TicTacToeGame {
 		return returnArray;
 	}
 
+	public boolean isOver(){
+		if(gameStatus == GameStatus.GAME_GOES_ON){
+			return false;
+		}
+		return true;
+	}
+	
+	public GameStatus getGameStatus(){
+		return gameStatus;
+	}
 }
