4bf7b853760559f8ffe8ecc71c35ddafe8ab2aa2
Burhan Aras <burhanaras@live.com>
2014-01-08 22:49:31 +0200

    Buttons are clickable anymore.

--- src/com/burhan/tictactoe/MainActivity.java
+++ src/com/burhan/tictactoe/MainActivity.java
@@ -2,12 +2,18 @@ package com.burhan.tictactoe;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private Button mBoardButtons[];
+	private TicTacToeGame mGame;
+	private TextView mInfoTextView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +21,36 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		initUI();
+		startNewGame();
 	}
 
 	private void initUI() {
+		mBoardButtons = new Button[TicTacToeGame.BOARD_SIZE];
+		mBoardButtons[0] = (Button) findViewById(R.id.button1);
+		mBoardButtons[1] = (Button) findViewById(R.id.button2);
+		mBoardButtons[2] = (Button) findViewById(R.id.button3);
+		mBoardButtons[3] = (Button) findViewById(R.id.button4);
+		mBoardButtons[4] = (Button) findViewById(R.id.button5);
+		mBoardButtons[5] = (Button) findViewById(R.id.button6);
+		mBoardButtons[6] = (Button) findViewById(R.id.button7);
+		mBoardButtons[7] = (Button) findViewById(R.id.button8);
+		mBoardButtons[8] = (Button) findViewById(R.id.button9);
+		mInfoTextView = (TextView) findViewById(R.id.information);
+		mGame = new TicTacToeGame();
 	}
 
+	private void startNewGame() {
+		mGame.clearBoard();
+		
+		/**/
+		for(int i=0;i<mBoardButtons.length;i++){
+			mBoardButtons[i].setText("");
+			mBoardButtons[i].setEnabled(true);
+			mBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+		}
+	}
+
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -27,4 +58,34 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	class ButtonClickListener implements OnClickListener{
+
+		private int location;
+
+		public ButtonClickListener(int location) {
+			this.location = location;
+		}
+
+		private void setMove(char player, int location) {
+			mGame.setMove(player, location);
+			updateView();
+		}
+
+		private void updateView() {
+			char board[] = mGame.getBoard();
+			for(int i=0; i<board.length;i++){
+				mBoardButtons[i].setText(String.valueOf(board[i]));
+				mBoardButtons[i].setTextColor(Color.rgb(0, 200, 0));
+			}
+		}
+
+		@Override
+		public void onClick(View v) {
+			if(mBoardButtons[location].isEnabled()){
+				setMove(TicTacToeGame.HUMAN_PLAYER, location);
+				mBoardButtons[location].setEnabled(false);
+			}
+		}
+		
+	}
 }
--- src/com/burhan/tictactoe/TicTacToeGame.java
+++ src/com/burhan/tictactoe/TicTacToeGame.java
@@ -1,5 +1,29 @@
 package com.burhan.tictactoe;
 
+
 public class TicTacToeGame {
+
+	public static final int BOARD_SIZE = 9;
+	public static final char HUMAN_PLAYER = 'X';
+	public static final char COMPUTER_PLAYER = 'O';
+	public static final char OPEN_SPOT = ' ';
+
+	private char board[] = new char[9];
+	
+	public void clearBoard() {
+	}
+
+	public void setMove(char player, int location) {
+		if(location >= board.length){
+			return;
+		}
+		if(board[location] == '\u0000'){
+			board[location] = player;
+		}
+	}
+
+	public char[] getBoard() {
+		return board;
+	}
 
 }
